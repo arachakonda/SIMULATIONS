9af6cf9b6c2fce1b85d5ddacf9a367473ab68521
arachakonda <ananthrachakonda@gmail.com>
2023-06-23 07:28:22 +0530

    added 2 scenes

--- pendulum/Single Pendulum/Manim/project/intro_video.py
+++ pendulum/Single Pendulum/Manim/project/intro_video.py
@@ -13,7 +13,18 @@ class Introduction(Scene):
             FadeIn(basel, shift=DOWN),
         )
         self.wait()
+        #self.play(FadeOut(basel, shift=UP))
 
+
+
+class Pendulum(SpaceScene):
+    def construct(self):
+        title = Tex(r"What are states?")
+        basel = MathTex(r"q \>\> \dot{q} \>\> \ddot{q}")
+        VGroup(title, basel).arrange(DOWN)
+        self.add(
+            title
+        )
         transform_title = Tex("Imagine a Pendulum")
         transform_title.to_corner(UP + LEFT)
         self.play(
@@ -21,13 +32,16 @@ class Introduction(Scene):
             LaggedStart(*[FadeOut(obj, shift=DOWN) for obj in basel]),
         )
         self.wait()
-
-class PendulumExample(SpaceScene):
-    def construct(self):
-        pends = VGroup(*[Pendulum(3)])
+        origin = ORIGIN+2*UP + 3*LEFT
+        pends = VGroup(*[Pendulum(3, pivot_point=origin)])
         self.add(pends)
         for p in pends:
             self.make_rigid_body(*p.bobs)
-            print(p.pivot_point)
+            verticalLine = DashedLine(start=origin, end=origin+2*DOWN, color=YELLOW_B)
+            self.add(verticalLine)
             p.start_swinging()
         self.wait(10)
+
+class PendulumText(scene):
+    def construct(self):
+        pass
